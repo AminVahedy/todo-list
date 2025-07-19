80b46d8ae07f69d76b95680e648431668463c3a8
AminVahedy <167294149+AminVahedy@users.noreply.github.com>
2025-07-19 18:02:42 +0330

    compleat project

--- src/MainBox.jsx
+++ src/MainBox.jsx
@@ -1,15 +1,19 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { useState } from "react";
 import Search from "./Search";
 import WorkItems from "./WorkItems";
 
 export default function MainBox() {
-  const [workList, setWorkList] = useState(
-    JSON.parse(localStorage.getItem("workList"))
-  );
+  // const [workList, setWorkList] = useState(function () {
+  //   const storageArr = JSON.parse(localStorage.getItem("workList"));
+  //   return storageArr;
+  // });
+  const [workList, setWorkList] = useState(() => {
+    return JSON.parse(localStorage.getItem("workList")) || [];
+  });
+
   const [quary, setQuary] = useState("");
   const inputRef = useRef(null);
-
   return (
     <main className="h-screen flex items-center justify-center">
       <div className="bg-[#1c1c1c] text-[#fff] w-[60%] h-[60%] rounded-lg p-[20px] flex flex-col items-center gap-[10px]">
